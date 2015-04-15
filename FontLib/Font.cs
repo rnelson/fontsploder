26109a4ce40843bd9be70c79b7ae9b0b8481cbd4
Ross Nelson <ross.nelson@gmail.com>
2015-04-15 12:36:47 -0500

    Added source code

---
+++ FontLib/Font.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace FontLib
+{
+    /// <summary>
+    /// Holds various information about a Font; for use with 
+    /// FontResource
+    /// </summary>
+    public class Font
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the font's filename
+        /// </summary>
+        public string Filename
+        {
+            get { return this.filename; }
+        }
+
+        /// <summary>
+        /// Gets the font's name; if unknown, it returns the 
+        /// filename
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        /// <summary>
+        /// Gets/sets the font's tag
+        /// </summary>
+        public Object Tag
+        {
+            get { return this.tag; }
+            set { this.tag = value; }
+        }
+        #endregion Properties
+
+        #region Members
+        private string filename;
+        private string name;
+        private object tag;
+        #endregion Members
+
+        #region Constructors
+        public Font(string filename)
+        {
+            // Save the filename
+            this.filename = filename;
+
+            // Fill in what else we can
+            this.ParseFont();
+        }
+        #endregion Constructors
+
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Private Methods
+        private void ParseFont()
+        {
+            // Set the name = filename
+            this.name = Path.GetFileName(this.Filename);
+
+            /*
+            ICollection<FontFamily> families = Fonts.GetFontFamilies(this.Filename);
+            foreach (FontFamily family in families)
+            {
+                family.
+            }
+            Fonts.GetTypefaces(
+            */
+        }
+        #endregion Private Methods
+    }
+}
---
+++ FontLib/FontResourceManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Win32;
+
+namespace FontLib
+{
+    /// <summary>
+    /// Uses AddFontResource() and RemoveFontResource() along 
+    /// with some internal data to temporarily add and remove 
+    /// fonts to the system
+    /// </summary>
+    public class FontResourceManager
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the list of fonts currently handled 
+        /// by this FontResource object
+        /// </summary>
+        public Font[] Fonts
+        {
+            get { return this.fonts.ToArray(); }
+        }
+
+        /// <summary>
+        /// Gets a filter (for use with OpenFileDialog/SaveFileDialog) of
+        /// the supported font types + "All Files (*.*)"
+        /// </summary>
+        public string SupportedTypesFileFilter
+        {
+            get { return "All Fonts (*.fon;*.fnt;*.ttf;*.ttc;*.fot;*.otf)|*.fon;*.fnt;*.ttf;*.ttc;*.fot;*.otf|Font Resource Files (*.fon)|*.fon|Bitmap Font Files (*.fnt)|*.fnt|TrueType Fonts (*.ttf)|*.ttf|TrueType Font Collections (*.ttc)|*.ttc|TrueType Resource Files (*.fot)|*.fot|OpenType Fonts (*.otf)|*.otf|All Files (*.*)|*.*"; }
+        }
+        #endregion Properties
+
+        #region Members
+        private List<Font> fonts;
+        #endregion Members
+
+        #region Constructors
+        public FontResourceManager()
+        {
+            // Allocate members
+            this.fonts = new List<Font>();
+        }
+        #endregion Constructors
+
+        #region Destructors
+        ~FontResourceManager()
+        {
+            this.RemoveAll();
+        }
+        #endregion Destructors
+
+        #region Public Methods
+        /// <summary>
+        /// Adds a font
+        /// </summary>
+        /// <param name="filename">the filename for the font</param>
+        /// <returns>0 on error, else the number of fonts added</returns>
+        public int Add(string filename)
+        {
+            // Add the font to our internal list
+            this.fonts.Add(new Font(filename));
+
+            // Add it to the system font resource table
+            return GDI32.AddFontResource(filename);
+        }
+
+        /// <summary>
+        /// Adds a font
+        /// </summary>
+        /// <param name="font">a FontLib.Font object representing the font</param>
+        /// <returns>0 on error, else the number of fonts added</returns>
+        public int Add(Font font)
+        {
+            // Add the font to our internal list
+            this.fonts.Add(font);
+
+            // Add it to the system font resource table
+            return GDI32.AddFontResource(font.Filename);
+        }
+
+        /// <summary>
+        /// Removes a font
+        /// </summary>
+        /// <param name="filename">the filename for the font</param>
+        /// <returns><c>true</c> on success, <c>false</c> on error</returns>
+        public bool Remove(string filename)
+        {
+            // Remove it from our list
+            Font font = this.fonts.Find(f => f.Filename == filename);
+            this.fonts.Remove(font);
+            
+            // Remove it from the system font resource table
+            return GDI32.RemoveFontResource(font.Filename);
+        }
+
+        /// <summary>
+        /// Removes all added fonts
+        /// </summary>
+        public void RemoveAll()
+        {
+            // Get an array of font names
+            Font[] list = this.Fonts;
+
+            // Call Remove() on each one
+            foreach (Font f in list)
+            {
+                this.Remove(f.Filename);
+            }
+        }
+        #endregion Public Methods
+    }
+}
